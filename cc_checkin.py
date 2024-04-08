34e2c97ea2ac2d6a1880f8142efeed3a87ac5c03
BlueCitizen <bluecitizens@hotmail.com>
2024-04-08 11:57:55 +0800

    url change

--- cc_checkin.py
+++ cc_checkin.py
@@ -30,7 +30,7 @@ def sign(cookie):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36",
         "Cookie": cookie,
     }
-    url = 'https://www.c-cloud.xyz/user/checkin'
+    url = 'https://www.cordc.net/user/checkin'
     s = json.dumps({})
     session = requests.session()
     response = requests.post(url, data=s, headers=headers)
@@ -42,5 +42,6 @@ def sign(cookie):
     return msg
 
 if __name__ == "__main__":
-    cookie = os.getenv("cc_cookie")
+    # cookie = os.getenv("cc_cookie")
+    cookie = "_ga=GA1.1.656736821.1709306419; uid=57729; email=bluecitizens%40163.com; key=83f78f2b1bcced496526637ea0de6e286bcaf2a396e1a; ip=cc7911a78e7fcdd5965de747ff4238c7; expire_in=1712634482; _ga_WDQMPH0H2X=GS1.1.1712548079.6.1.1712548084.55.0.0; crisp-client%2Fsession%2Ff24e0785-07d5-4a5f-961b-bde1c9b6245b=session_b037b4aa-e2bd-4b00-a01f-4a0d6e208f12"
     push(sign(cookie))
